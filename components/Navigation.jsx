cf9acf1536d6d0131fe1e7c5107cbe1248fae14c
Abhinav Choudhary <abhinavchdhary@gmail.com>
2024-09-12 12:43:21 +0530

    Add: a basic navigation

---
+++ components/Navigation.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to='/'>Game</Link>
+        </li>
+
+        <li>
+          <Link to='/leaderboard'>Leaderboard</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import React from "react";
-import SnakeGame from "../components/SnakeGame";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import GamePage from "../pages/GamePage";
+import LeaderBoard from "../pages/LeaderBoard";
+import About from "../pages/About";
+import Navigation from "../components/Navigation";
 
 export default function App() {
   return (
-    <>
-      <SnakeGame
-        snakeColor='green'
-        appleColor='red'
-        percentageWidth={50}
-        startSnakeSize={6}
-      />
-    </>
+    <Router>
+      <div>
+        <Navigation/>
+        <Routes>
+          <Route path='/' element={<GamePage />} />
+          <Route path='/leaderboard' element={<LeaderBoard />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
