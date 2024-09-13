7d1e79a0a646c179324eb140a40901eaa1793b7f
Abhinav Choudhary <abhinavchdhary@gmail.com>
2024-09-13 14:56:00 +0530

    Fix: Game Over appears in center now

--- src/components/SnakeGame.jsx
+++ src/components/SnakeGame.jsx
@@ -35,18 +35,18 @@ function SnakeGame({
     };
   }, [handleKeyDown]);
 
-  // Render the game over screen if the game is over
-  if (isGameOver) {
-    return (
-      <GameOver
-        width={width}
-        height={height}
-        highScore={highScore}
-        newHighScore={newHighScore}
-        score={score}
-      />
-    );
-  }
+  // // Render the game over screen if the game is over
+  // if (isGameOver) {
+  //   return (
+  //     <GameOver
+  //       width={width}
+  //       height={height}
+  //       highScore={highScore}
+  //       newHighScore={newHighScore}
+  //       score={score}
+  //     />
+  //   );
+  // }
 
   // Render the game board
   return (
@@ -58,32 +58,46 @@ function SnakeGame({
         borderWidth: width / 50,
       }}
     >
-      {snake.map((snakePart, index) => (
-        <div
-          key={index}
-          className='Block'
-          style={{
-            width: blockWidth,
-            height: blockHeight,
-            left: snakePart.Xpos,
-            top: snakePart.Ypos,
-            backgroundColor: snakeColor,
-          }}
-        />
-      ))}
-      <div
-        className='Block'
-        style={{
-          width: blockWidth,
-          height: blockHeight,
-          left: apple.Xpos,
-          top: apple.Ypos,
-          backgroundColor: appleColor,
-        }}
-      />
-      <div id='Score' style={{ fontSize: width / 20 }}>
-        HIGH-SCORE: {highScore} &emsp; SCORE: {score}
-      </div>
+      {isGameOver ? (
+        <>
+          <GameOver
+            width={width}
+            height={height}
+            highScore={highScore}
+            newHighScore={newHighScore}
+            score={score}
+          />
+        </>
+      ) : (
+        <>
+          {snake.map((snakePart, index) => (
+            <div
+              key={index}
+              className='Block'
+              style={{
+                width: blockWidth,
+                height: blockHeight,
+                left: snakePart.Xpos,
+                top: snakePart.Ypos,
+                backgroundColor: snakeColor,
+              }}
+            />
+          ))}
+          <div
+            className='Block'
+            style={{
+              width: blockWidth,
+              height: blockHeight,
+              left: apple.Xpos,
+              top: apple.Ypos,
+              backgroundColor: appleColor,
+            }}
+          />
+          <div id='Score' style={{ fontSize: width / 20 }}>
+            HIGH-SCORE: {highScore} &emsp; SCORE: {score}
+          </div>
+        </>
+      )}
     </div>
   );
 }
