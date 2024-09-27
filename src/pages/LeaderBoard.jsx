afc2c3c1979d87c1150e6f91aeac2128c0d2f221
Abhinav Choudhary <abhinavchdhary@gmail.com>
2024-09-27 08:56:40 +0530

    Updt: Only take top 7 users

--- src/pages/LeaderBoard.jsx
+++ src/pages/LeaderBoard.jsx
@@ -11,7 +11,7 @@ export default function LeaderBoard() {
         method: "GET",
       });
       const data = await response.json();
-      setLeaders(data);
+      setLeaders(data.slice(0, 7));
     } catch (error) {
       console.log(error);
     }
@@ -24,24 +24,24 @@ export default function LeaderBoard() {
   return (
     <>
       <Navigation />
-      <div className="max-w-4xl mx-auto my-10 p-4 bg-gray-800 text-white rounded-lg shadow-lg">
-        <h1 className="text-4xl font-bold mb-4 text-center">LeaderBoard</h1>
+      <div className='max-w-4xl mx-auto my-10 p-4 bg-gray-800 text-white rounded-lg shadow-lg mb-9'>
+        <h1 className='text-4xl font-bold mb-4 text-center'>LeaderBoard</h1>
         {leaders.length > 0 ? (
-          <ul className="space-y-4">
+          <ul className='space-y-4'>
             {leaders.map((leader, index) => (
               <li
                 key={index}
-                className="flex justify-between bg-gray-700 p-4 rounded-md shadow-md"
+                className='flex justify-between bg-gray-700 p-4 rounded-md shadow-md'
               >
-                <span className="text-xl font-semibold">{leader.username}</span>
-                <span className="text-xl text-yellow-400">
+                <span className='text-xl font-semibold'>{leader.username}</span>
+                <span className='text-xl text-yellow-400'>
                   {leader.highScore}
                 </span>
               </li>
             ))}
           </ul>
         ) : (
-          <p className="text-center text-lg text-gray-400">
+          <p className='text-center text-lg text-gray-400'>
             Please come back later
           </p>
         )}
